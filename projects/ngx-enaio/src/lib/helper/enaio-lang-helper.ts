9ae7f353b084e970750c16e14bd3bbaa6c8ad4f3
Ulrich Wohlfeil <ulrich.wohlfeil@ecmind.ch>
2019-11-22 14:44:57 +0100

    Version 0.0.15

--- projects/ngx-enaio/src/lib/helper/enaio-lang-helper.ts
+++ projects/ngx-enaio/src/lib/helper/enaio-lang-helper.ts
@@ -2,42 +2,41 @@
  * Helper Class for enaio Language Information
  */
 export class EnaioLangHelper {
-  private static isoToIdMap = {
-    DE: '7',
-    EN: '9',
-    SP: '10',
-    FR: '12',
-    HU: '14',
-    IT: '16',
-    NL: '19',
-    PL: '21',
-    SE: '29'
-  };
-  private static idToIsoMap = {
-    7: 'DE',
-    9: 'EN',
-    10: 'SP',
-    12: 'FR',
-    14: 'HU',
-    16: 'IT',
-    19: 'NL',
-    21: 'PL',
-    29: 'SE'
-  };
-
   /**
    * @param id Language id like 7 for german
    * @returns ISO code for the given lang_id
    */
-  public getIDForIso(id: string) {
-    return EnaioLangHelper.isoToIdMap[id];
+  static getIdForIso(id: string): string {
+    const isoToIdMap = {
+      DE: '7',
+      EN: '9',
+      SP: '10',
+      FR: '12',
+      HU: '14',
+      IT: '16',
+      NL: '19',
+      PL: '21',
+      SE: '29'
+    };
+    return isoToIdMap[id];
   }
 
   /**
    * @param iso ISO code like DE for german
    * @returns Lang_id for the ISO code
    */
-  public getIsoForId(iso: string) {
-    return EnaioLangHelper.idToIsoMap[iso];
+  static getIsoForId(iso: string): string {
+    const idToIsoMap = {
+      7: 'DE',
+      9: 'EN',
+      10: 'SP',
+      12: 'FR',
+      14: 'HU',
+      16: 'IT',
+      19: 'NL',
+      21: 'PL',
+      29: 'SE'
+    };
+    return idToIsoMap[iso];
   }
 }
